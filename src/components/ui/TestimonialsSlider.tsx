2e1d564eb0aea3b67e6f0ab9297f8cac1dbc97f2
Sandeep Kumar <sandeepkhoj@gmail.com>
2026-05-11 18:08:36 +0530

    fix: swiper navigation CSS import path, hero transition-delay

--- src/components/ui/TestimonialsSlider.tsx
+++ src/components/ui/TestimonialsSlider.tsx
@@ -2,7 +2,7 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Autoplay, A11y } from 'swiper/modules';
 import 'swiper/css';
-import 'swiper/css/navigation';
+import 'swiper/modules/navigation.css';
 
 interface Testimonial {
   quote: string;
